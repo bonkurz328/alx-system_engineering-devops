dc54b647e547a1a404015c9c995b101ff4b45912
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-08-05 13:50:15 +0200

    Update 1-hello_you.sh

--- 0x03-shell_variables_expansions/1-hello_you.sh
+++ 0x03-shell_variables_expansions/1-hello_you.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 echo "hello $USER"
-
-uid=1000(julien) gid=1000(julien) groups=1000(julien),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),113(lpadmin),128(sambashare)
-julien@ubuntu:/tmp/0x03$ ./1-hello_you 
-hello julien
